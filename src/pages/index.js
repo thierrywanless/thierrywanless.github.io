94e81c21cb684ea215db9f077a546ece9304278d
thierrywanless@gmail.com <thierrywanless@gmail.com>
2021-01-13 21:27:41 +0000

    Updated header to include hero; Added header image for hero; Added font-awesome icons for hero social links;

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
 import * as React from "react";
+import Header from "../components/header";
 
 import Layout from "../components/layout";
 
 const IndexPage = () => {
   return (
     <Layout>
+      <Header />
       <h1 id="about">About</h1>
       <h1 id="skills">Skills</h1>
       <h1 id="projects">Projects</h1>
