7a3b1c6b70aaa321e5b21ca93a2d778e81d41118
thierrywanless@gmail.com <thierrywanless@gmail.com>
2021-01-14 15:49:19 +0000

    Added about starter

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,14 @@
 import * as React from "react";
-import Header from "../components/header";
 
+import About from "../components/about";
+import Header from "../components/header";
 import Layout from "../components/layout";
 
 const IndexPage = () => {
   return (
     <Layout>
       <Header />
-      <h1 id="about">About</h1>
+      <About />
       <h1 id="skills">Skills</h1>
       <h1 id="projects">Projects</h1>
       <h1 id="contact">Contact</h1>
